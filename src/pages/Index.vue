574061950712e0c7cfcec655c1d13d88b7577c3d
Humboldt155 <petr_kozyrev@icloud.com>
2018-07-29 01:39:31 +0300

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page>
+    <br>
+    <q-search
+      v-model="searchModel"
+      :debounce="600"
+      placeholder="Например, «Скалодром»"
+      icon="fas fa-search"
+      float-label="Начать поиск"
+    />
+    <br>
+    <h1>Sport</h1>
+    <h3 class="beta">beta</h3>
+  </q-page>
+</template>
+
+<style>
+  .beta {
+    color: darkviolet;
+  }
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
