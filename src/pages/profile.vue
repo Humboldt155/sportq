7bd720ec245c84c2e5794b94ad49d87213d4f51e
Humboldt155 <petr_kozyrev@icloud.com>
2018-07-30 08:30:50 +0300

    2

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,6 +1,29 @@
 <template>
   <q-page>
-    Профиль
+    <h4>
+      <div>Петр Козырев</div>
+    </h4>
+    <div class="row">
+      <div>информация</div>
+    </div>
+    <div class="row">
+      <div>фото</div>
+    </div>
+    <div class="row">
+      <div>настройи</div>
+    </div>
+    <div class="row">
+      <div>Ссылки на соц. сети</div>
+    </div>
+    <div class="row">
+      <div>Моя статистика</div>
+    </div>
+    <div class="row">
+      <div>Настроийи видимости</div>
+    </div>
+    <div class="row">
+      <div>выход</div>
+    </div>
   </q-page>
 </template>
 
